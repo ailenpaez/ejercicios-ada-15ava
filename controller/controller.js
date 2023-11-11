8c5dd79412db3404507bce6c365c1120f220fe14
ailenpaez <a.jorgelinapaez@gmail.com>
2023-11-10 22:00:14 -0300

    Modificaciones en server para conectar el módulo controller y modificación en las funciones del controller

--- controller/controller.js
+++ controller/controller.js
@@ -1,10 +1,39 @@
-const m1 = (n1,n2) => {
-    return n1 + n2;
+const m1 = (n1, n2) => {
+  return n1 + n2;
 };
 
-const m2 = () => {};
-const m3 = () => {};
-const m4 = () => {};
+const m2 = (n) => {
+  return n ** 2;
+};
+
+//3. Implementa una función que tome un array de números y devuelva la suma de todos los elementos.
+const m3 = (arrayNumeros) => {
+  let suma = 0;
+  arrayNumeros.forEach((numero) => {
+    suma += numero;
+  });
+  return suma;
+};
+
+//4. Crea una función que reciba un string y devuelva el número de vocales que contiene.
+const m4 = (string) => {
+    let vocales = 0
+    for(let letra of string){
+        if (
+            letra === "a" ||
+            letra === "e" ||
+            letra === "i" ||
+            letra === "o" ||
+            letra === "u"
+          ) {
+              vocales += 1
+          }
+    }
+
+  return vocales
+    
+};
+
 const m5 = () => {};
 const m6 = () => {};
 const m7 = () => {};
@@ -12,4 +41,4 @@ const m8 = () => {};
 const m9 = () => {};
 const m10 = () => {};
 
-export{m1}
+export { m1, m2, m3, m4};
--- controller/server.js
+++ controller/server.js
@@ -1,6 +1,7 @@
 import net from "node:net";
 import dotenv from "dotenv";
-import{m1} from "./controller.js"
+import{m1, m2 ,m3, m4} from "./controller.js"
+
 dotenv.config();
 
 const port = process.env.PORT;
@@ -8,6 +9,19 @@ const port = process.env.PORT;
 const serverTCP = net.createServer();
 
 const processRequest = (request) =>{
+    const r = request
+    const module = request[0]
+    switch(module){
+        case "m1":
+            return m1(Number(r[1]),Number(r[2]));
+        case "m2":
+            return m2(Number(r[1]));
+        case "m3":
+            return m3(JSON.parse(r[1]));
+        case "m4":
+            return m4(r[1]);
+
+    }
 }
 
 serverTCP.on("connection", (socket) =>{
@@ -18,7 +32,7 @@ serverTCP.on("connection", (socket) =>{
 
     socket.on("data", (bufferDataClient)=>{
     const data = JSON.parse(bufferDataClient.toString())
-    const response = processRequest()
+    const response = processRequest(data)
     socket.write(JSON.stringify(response))
     })
 
