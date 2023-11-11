c2684644e563d3feb9ed1494e4f6830695e87e4a
ailenpaez <a.jorgelinapaez@gmail.com>
2023-11-11 02:52:25 -0300

    Nuevas modificaciones en las funciones del controller y en el switch del server

--- controller/controller.js
+++ controller/controller.js
@@ -17,28 +17,75 @@ const m3 = (arrayNumeros) => {
 
 //4. Crea una función que reciba un string y devuelva el número de vocales que contiene.
 const m4 = (string) => {
-    let vocales = 0
-    for(let letra of string){
-        if (
-            letra === "a" ||
-            letra === "e" ||
-            letra === "i" ||
-            letra === "o" ||
-            letra === "u"
-          ) {
-              vocales += 1
-          }
+  let vocales = 0;
+  for (let letra of string) {
+    if (
+      letra === "a" ||
+      letra === "e" ||
+      letra === "i" ||
+      letra === "o" ||
+      letra === "u"
+    ) {
+      vocales += 1;
     }
+  }
 
-  return vocales
-    
+  return vocales;
+};
+// 5. Escribe una función que tome un string de elementos separados por guiones y devuelva el array ordenado alfabéticamente.
+
+const m5 = (string) => {
+  let stringConGuiones = string.split("-");
+  let stringOrdenado = stringConGuiones.sort();
+  return stringOrdenado
 };
 
-const m5 = () => {};
-const m6 = () => {};
-const m7 = () => {};
-const m8 = () => {};
-const m9 = () => {};
+// 6. Define una función que tome un array de cualquier tipo y devuelva el primer elemento.
+const m6 = (arrayIndistinto) => {
+  if (arrayIndistinto.length > 0){
+    return arrayIndistinto[0]
+  }else if (arrayIndistinto.length === 0 ){
+    return null
+  } //sólo funciona con nros (?)
+
+};
+
+// 7. Implementa una función que tome un número como parámetro y devuelva su factorial.
+const m7 = (num) => {
+  if (num < 0){
+    return 'No se puede calcular el factorial de un nro negativo'
+  }
+  
+  if (num === 0 || num === 1){
+    return 1
+  }
+
+  let factorial = 1
+  for (let i = 2; i <= num; i++){
+    factorial *=i
+  }
+  return factorial
+};
+
+
+// 8. Escribe una función que tome un objeto como parámetro e imprima todas sus propiedades y valores.
+// const m8 = (objeto) => {
+
+// };
+
+// 9. Implementa una función que tome un array de números y devuelva el número más grande.
+const m9 = (arrayNumeros) => {
+  let numeroMayor = 0;
+  for (let i = 1; i < arrayNumeros.length; i++) {
+    if (arrayNumeros[i] > numeroMayor) {
+      numeroMayor = arrayNumeros[i];
+    }
+  }
+  return numeroMayor;
+};
+
+// 10. Crea una función que tome un string como parámetro y devuelva true si es un palíndromo, o false en caso contrario.
 const m10 = () => {};
 
-export { m1, m2, m3, m4};
+export { m1, m2, m3, m4 , m5, m6, m7, m8, m9, m10};
+
--- controller/server.js
+++ controller/server.js
@@ -1,6 +1,6 @@
 import net from "node:net";
 import dotenv from "dotenv";
-import{m1, m2 ,m3, m4} from "./controller.js"
+import { m1, m2, m3, m4, m5, m6, m7,m9} from "./controller.js";
 
 dotenv.config();
 
@@ -8,38 +8,44 @@ const port = process.env.PORT;
 
 const serverTCP = net.createServer();
 
-const processRequest = (request) =>{
-    const r = request
-    const module = request[0]
-    switch(module){
-        case "m1":
-            return m1(Number(r[1]),Number(r[2]));
-        case "m2":
-            return m2(Number(r[1]));
-        case "m3":
-            return m3(JSON.parse(r[1]));
-        case "m4":
-            return m4(r[1]);
+const processRequest = (request) => {
+  const r = request;
+  const module = request[0];
+  switch (module) {
+    case "m1":
+      return m1(Number(r[1]), Number(r[2]));
+    case "m2":
+      return m2(Number(r[1]));
+    case "m3":
+      return m3(JSON.parse(r[1]));
+    case "m4":
+      return m4(r[1]);
+    case "m5":
+      return m5(r[1])
+    case "m6":
+      return m6(JSON.parse(r[1]))
+    case "m7":
+      return m7(Number(r[1]));
+    // case "m8":
+    //   return m8(JSON.parse(JSON.stringify(r[1])))
+    case "m9":
+      return m9(JSON.parse(r[1]))}
+};
 
-    }
-}
+serverTCP.on("connection", (socket) => {
+  socket.on("close", () => {
+    console.log("Client disconnected!");
+  });
 
-serverTCP.on("connection", (socket) =>{
+  socket.on("data", (bufferDataClient) => {
+    const data = JSON.parse(bufferDataClient.toString());
+    const response = processRequest(data);
+    socket.write(JSON.stringify(response));
+  });
 
-    socket.on("close", ()=>{
-        console.log("Client disconnected!");
-    })
+  console.log("Client Connected!");
+});
 
-    socket.on("data", (bufferDataClient)=>{
-    const data = JSON.parse(bufferDataClient.toString())
-    const response = processRequest(data)
-    socket.write(JSON.stringify(response))
-    })
-
-    console.log("Client Connected!");
-})
-
-
-serverTCP.listen(port, () =>{
-    console.log("connection successfull!");
-})
+serverTCP.listen(port, () => {
+  console.log("connection successfull!");
+});
